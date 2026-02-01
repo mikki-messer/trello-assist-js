aa01a3706f46b28172131b2789aea789e746c5ac
Mike Bulanov <mikki.messer@gmail.com>
2026-02-01 15:08:27 +0400

    Added resolving projectName by custom field Id &  customFieldItem.Id Value

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const { incrementProjectCounter } = require('./db');
+const { getProjectNameFromIdValue } = require('./trello-utils');
+
+const TRELLO_CUSTOM_FIELD_NAME = process.env.TRELLO_CUSTOM_FIELD_NAME;
+const TRELLO_EVENT_TYPE = process.env.TRELLO_EVENT_TYPE;
 
 //Middleware for the JSON parsing
 app.use(express.json());
@@ -16,12 +21,45 @@ app.get('/webhook', (req, res) => {
 })
 
 //Endpoint for the Trello webhooks
-app.post('/webhook', (req, res) => {
-    console.log('Webhook received');
-    console.log('Event type:', req.body.action.type);
-    //console.log(req.body);
-    console.log(JSON.stringify(req.body.action.data, null, 2));
+app.post('/webhook', async (req, res) => {
+    console.log('Webhook received!');
+
+    const eventType = req.body.action.type;
+    console.log('Event type:', eventType);
+
+    
+    if (eventType === TRELLO_EVENT_TYPE) {
+        try {
+            let customField = req.body.action.data.customField;
+            let customFieldItem = req.body.action.data.customFieldItem;
+            let card = req.body.action.data.card;
+
+            if (customField.name === TRELLO_CUSTOM_FIELD_NAME) {
+                console.log(`Project field changed!`);
+                console.log(`Card name:`, card.name);
+
+                let projectName = await getProjectNameFromIdValue(
+                    customField.id, 
+                    customFieldItem.idValue);
+
+                //TODO: Get project name
+                console.log('idValue:', projectName);
+
+                if (projectName) {
+                    //increasing counter
+                    let newNumber = await incrementProjectCounter(projectName);
+                    console.log(`New number for ${projectName}:`, newNumber);
+                }
+                
+            }
+        } catch (error) {
+            console.error('Error processing webhook:', error.message);
+            //TODO: update card name
+        }
+    }
+
     res.status(200).send('OK');
+    
 });
 
 app.get('/', (req, res) => {
@@ -29,6 +67,7 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ trello-utils.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const API_KEY = process.env.TRELLO_API_KEY;
+const TOKEN = process.env.TRELLO_TOKEN;
+const CUSTOM_FIELDS_URL = process.env.TRELLO_CUSTOM_FIELDS_URL;
+
+//cache for keeping customFieldOptions
+let customFieldOptionsCache = {};
+
+//fetch customFieldOptions
+async function getCustomFieldOptions(customFieldId) {
+    //return if already cached
+    if (customFieldOptionsCache[customFieldId]){
+        return customFieldOptionsCache[customFieldId];
+    }
+    
+    try {
+        const response = await axios.get(
+            CUSTOM_FIELDS_URL.concat(customFieldId),
+            { params: { key: API_KEY, token: TOKEN } }
+        );
+
+        //creating mapping id -> text
+        const optionsMap = {};
+        response.data.options.forEach(option => {
+            optionsMap[option.id] = option.value.text;
+        });
+
+        //saving to cache
+        customFieldOptionsCache[customFieldId] = optionsMap;
+
+        return optionsMap;
+    }
+    catch (error) {
+        console.error('Error fetching custom field options:', error.message);
+        return {};
+    }
+}
+
+//getting field value name by idValue
+async function getProjectNameFromIdValue(customFieldId, idValue) {
+    const options = await getCustomFieldOptions(customFieldId);
+    return options[idValue] || null;
+}
+
+module.exports = {
+    getProjectNameFromIdValue
+};
